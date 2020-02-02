36941997d009ae0b9aa74a203b7044d8796d1a32
Alvaro Rodrigues <alvarorodrigues1710@gmail.com>
2020-02-02 20:30:56 +0000

    first commit

---
+++ src/main/java/microservicescourse/netflizzuulapigatewayserver/NetflizZuulApiGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package microservicescourse.netflizzuulapigatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflizZuulApiGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflizZuulApiGatewayServerApplication.class, args);
+	}
+
+}
